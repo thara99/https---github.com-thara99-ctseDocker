e03617648c29229f2d1473337cea02ce6b872e12
thara99 <it19148014@my.sliit.lk>
2022-05-17 20:33:12 +0530

    add build

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ mongoose.connect(URI, {
 })
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8089
 app.listen(PORT, () =>{
     console.log('Server is running on port', PORT)
 })
